d75d7371c1c4f739ba4be15056272d05387bf826
KamariFlintroy <kamariflintroy@gmail.com>
2024-04-08 14:19:57 -0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+let favoriteFood = 'Pasta'
+document.write(favoriteFood);
+favoriteFood = 'Pasta'
+
+alert('Hello World');
