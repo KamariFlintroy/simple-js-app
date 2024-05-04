e24aaf6fcea81f61c1192869aadbd59992eb3ce2
KamariFlintroy <kamariflintroy@gmail.com>
2024-05-04 18:45:02 -0400

    Added Modals

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 let pokemonRepository = (function () {
-let pokemonList= [];
-let apiUrl= 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+    let modalContainer = document.querySelector('#modal-container')
         function add(pokemon) {
             pokemonList.push(pokemon);
         }
@@ -16,12 +17,32 @@ let apiUrl= 'https://pokeapi.co/api/v2/pokemon/?limit=150';
             button.addEventListener('click', function(){showDetails(pokemon);});
         }
 
+        // Get the button:
+        let mybutton = document.getElementById("myBtn");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {scrollFunction()};
+
+        function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+         mybutton.style.display = "none";
+        }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
+
         function loadList() {
             return fetch(apiUrl).then(function (response) {
                 return response.json();
             }).then(function (json) {
                 json.results.forEach(function (item) {
-                    let pokemon= {
+                    let pokemon = {
                         name: item.name,
                         detailsUrl: item.url
                     };
@@ -37,9 +58,9 @@ let apiUrl= 'https://pokeapi.co/api/v2/pokemon/?limit=150';
             return fetch(url).then(function (response) {
                 return response.json();
             }).then(function (details) {
-                item.imageUrl=details.sprites.front_default;
-                item.height=details.height;
-                item.types=details.types;
+                item.imageUrl = details.sprites.front_default;
+                item.height = details.height;
+                item.types = details.types;
             }).catch(function (e) {
                 console.error(e);
             });
@@ -47,9 +68,91 @@ let apiUrl= 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
         function showDetails(pokemon) {
             loadDetails(pokemon).then(function() {
-            console.log(pokemon);
+            showModal(pokemon.name, pokemon.height, pokemon.imageUrl);
             });
         }
+
+        function showModal(name, height, imageUrl) {
+            modalContainer.innerHTML ='';
+            let modal = document.createElement('div');
+            modal.classList.add('modal');
+            let closeButtonElement = document.createElement('button');
+            closeButtonElement.classList.add('modal-close');
+            closeButtonElement.innerText = 'Close';
+            closeButtonElement.addEventListener('click', hideModal);
+            let titleElement = document.createElement('h1');
+            titleElement.innerText = name;
+            let imageContainer = document.createElement('div');
+            imageContainer.classList.add('image');
+            let imageElement = document.createElement('img');
+            imageElement.src = imageUrl;
+            let contentElement  = document.createElement('p');
+            contentElement.innerText = 'Height: ';
+            contentElement.innerHTML += height;
+            modal.appendChild(closeButtonElement);
+            modal.appendChild(titleElement);
+            modal.appendChild(imageContainer);
+            imageContainer.appendChild(imageElement);
+            modal.appendChild(contentElement);
+            modalContainer.appendChild(modal);
+            modalContainer.classList.add('is-visible');
+        }
+
+    document.querySelector('#show-modal').addEventListener('click', () => {
+        showModal('Modal tite', 'This is the content');
+    });
+
+    modalContainer.addEventListener('click', (e) => {
+        let target = e.target;
+        if (target === modalContainer) {
+            hideModal();
+        }
+    })
+
+    let dialogPromiseReject;
+
+    function hideModal() {
+        modalContainer.classList.remove('is-visible');
+        if (dialogPromiseReject) {
+            dialogPromiseReject();
+            dialogPromiseReject = null;
+        }
+    }
+
+    function showDialog() {
+        let modal = modalContainer.querySelector('.modal');
+        let confirmButton = document.createElement('button');
+        confirmButton.classList.add('modal-confirm');
+        confirmButton.innerText = 'Confirm';
+        let cancelButton = document.createElement('button');
+        cancelButton.classList.add('modal-cancel');
+        cancelButton.innerText = 'Cancel';
+        modal.appendChild(confirmButton);
+        modal.appendChild(cancelButton);
+        confirmButton.focus();
+        return new Promise((resolve, reject) => {
+            cancelButton.addEventListener('click', hideModal);
+            confirmButton.addEventListener('click', () => {
+                dialogPromiseReject = null;
+                hideModal();
+                resolve();
+            });
+            dialogPromiseReject = reject;
+        });
+    }
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape' && modalContainer.classList.contains(is-visible)) {
+            hideModal();
+        }
+    })
+
+    document.querySelector('#show-dialog').addEventListener('click', () => {
+        showDialog('Confirm action', 'Are you sure you want to do this?').then(function() {
+            alert('confirmed!');
+        }, () => {
+            alert('not confirmed');
+        });
+    });
         
         function getAll() {
             return pokemonList;
@@ -59,7 +162,9 @@ let apiUrl= 'https://pokeapi.co/api/v2/pokemon/?limit=150';
             getAll: getAll,
             addListitem: addListitem,
             loadList: loadList,
-            loadDetails: loadDetails
+            loadDetails: loadDetails,
+            showDetails: showDetails,
+            scrollFunction: scrollFunction,
         };
 }) ();
 
